61e15d886b6a9b1ce6acbb7b1aa249f821036b5c
Yeseul Chung <amy7806@gmail.com>
2022-10-06 16:57:44 +0900

    [unrated] Title: 짝수의 합, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.swift
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.swift
@@ -7,5 +7,10 @@ func solution(_ n:Int) -> Int {
             sum += num
         }
     }
+    /*
+    for num in 1...n where num % 2 == 0 {
+        sum += num
+    } --> 이것도 가능
+    */
     return sum
 }
