ebac1badde5286e7af5980379ca2143df72dd7cd
Yeseul Chung <amy7806@gmail.com>
2022-10-12 21:05:57 +0900

    [Silver III] Title: 소수 구하기, Time: 12 ms, Memory: 2996 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -1,24 +1,23 @@
 #include <iostream>
+#include <cmath>
+
 #define MAX 1000001
 using namespace std;
 
-int n, m;
 bool arr[MAX];
+int M, N;
 
-int main(){
-    ios::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
-    cin >> m >> n;
+int main(void){
+    cin >> M >> N;
     arr[0] = arr[1] = true;
-    for(int i=2;i*i<MAX;i++){
-        if(arr[i])continue;
-        for(int j=i*i;j<MAX;j+=i){
+    for(int i = 2; i * i <= MAX; i++){
+        if(arr[i]) continue;
+        for(int j = i * i; j < MAX; j += i){
             arr[j] = true;
         }
     }
-    for(int i=m;i<=n;i++){
-        if(!arr[i]){
-            cout<<i<<"\n";
-        }
+    for(int i = M; i <= N; i++){
+        if(!arr[i]) cout << i << '\n';
     }
     return 0;
 }
