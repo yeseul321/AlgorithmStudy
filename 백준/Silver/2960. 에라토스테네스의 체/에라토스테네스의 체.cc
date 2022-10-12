d5516fa387ad3e81dedaaf28e6aec316a85bfc3e
Yeseul Chung <amy7806@gmail.com>
2022-10-12 21:22:18 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.cc
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.cc
@@ -1,31 +1,26 @@
 #include <iostream>
 #include <cmath>
 
-#define MAX 10001
-
+#define MAX 1001
 using namespace std;
 
+int N, K, count;
+
 int main(void){
-    int n, k, num = 0;
-    cin >> n >> k;
-    int arr[MAX];
-    
-    for(int i = 2; i <= n; i++){
-        arr[i] = 1;
-    }
-    
-    for(int i = 2; i <= n; i++){
-        for(int j = 1; j * i <= n; j++){
-            if(arr[i * j] == 1){
-                arr[i * j] = 0;
-                num++;
-                
-                if(num == k){
-                    cout << i * j;
-                    return 0;
-                }
+    cin >> N >> K;
+    bool arr[N+1];
+    for(int i = 0; i <= N; i++) arr[i] = true;
+    for(int i = 2; i <= N; i++){
+        if(arr[i] == false) continue;
+        for(int j = 1; i * j <= N; j++){
+            if(arr[i * j] == true){
+                arr[i * j] = false;
+                count++;
+            }
+            if(count == K){
+                cout << i * j;
+                return 0;
             }
         }
     }
-   
 }
