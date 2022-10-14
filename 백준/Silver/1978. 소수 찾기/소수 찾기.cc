1a20d57d944d74355daa5b5bee9f909ae5ec1259
Yeseul Chung <amy7806@gmail.com>
2022-10-14 22:35:25 +0900

    [Silver V] Title: 소수 찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.cc
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.cc
@@ -1,34 +1,24 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
+bool arr[1001];
 
-int num = 0, cnt = 0;
-int prm = 0;
-
-void isPrime(int n) {
-    if (n == 1) {
-        cnt--;
-        return;
-    }
-    for (int j = 2; j <= sqrt(n); j++)
-    {
-        if (n % j == 0) {
-            cnt--;
-            break;
+int main(void){
+    arr[0] = arr[1] = true;
+    for(int i = 2; i <= 1001; i++){
+        if(!arr[i]){
+            for(int j = 2; i * j < 1001; j++){
+                arr[i * j] = true;
+            }
         }
     }
-}
-
-int main(void) {
-    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> num;
-    cnt = num;
-    for (int i = 0; i < num; i++) {
-        cin >> prm;
-        isPrime(prm);
+    int cnt = 0;
+    int N = 0;
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        int num = 0;
+        cin >> num;
+        if(!arr[num]) cnt++;
     }
     cout << cnt;
-
-    return 0;
 }
