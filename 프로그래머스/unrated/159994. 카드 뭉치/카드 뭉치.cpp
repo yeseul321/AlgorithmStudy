881ac914df84ae561a29c71b4b6d7f403ffc89e9
Yeseul Chung <amy7806@gmail.com>
2023-02-22 01:32:48 +0900

    [unrated] Title: 카드 뭉치, Time: 0.01 ms, Memory: 3.6 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.cpp
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.cpp
@@ -4,32 +4,26 @@
 using namespace std;
 
 string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
-    string answer = "";
-    string myAnswer = "";
-    int first = 0, second = 0, goalCount = 0;
-    
-    for(int i = 0; i < goal.size(); i++){
-        answer += goal[i];
-    }
-    
-    while(true) {
-        if((first != cards1.size()) && cards1[first] == goal[goalCount]){
-            first++;
-            myAnswer += goal[goalCount];
-            goalCount++;
-        } else if((second != cards2.size()) && cards2[second] == goal[goalCount]) {
-            second++;
-            myAnswer += goal[goalCount];
-            goalCount++;
-        } else {
+    string answer = "Yes";
+
+    int i1 = 0;
+    int i2 = 0;
+
+    for(const auto& v : goal)
+    {
+        if(i1 != cards1.size() && cards1[i1] == v)
+        {
+            i1++;
+        }
+        else if(i2 != cards2.size() && cards2[i2] == v)
+        {
+            i2++;
+        }
+        else
+        {
             answer = "No";
             break;
         }
-        if(answer == myAnswer) { 
-            answer = "Yes";
-            break;
-        }
     }
-    if(myAnswer.size() == 0) answer = "No";
     return answer;
 }
